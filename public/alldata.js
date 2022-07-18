03353e55424c68eb3b9182a01718b3a2bea29983
Bishar Mohamed <69430722+bishar08@users.noreply.github.com>
2022-07-18 17:59:21 -0500

    Update alldata.js

--- public/alldata.js
+++ public/alldata.js
@@ -1,21 +1,47 @@
-const { setEnvironmentData } = require('worker_threads')
+// const { setEnvironmentData } = require('worker_threads')
 
 function AllData() {
   const [data, setData] = React.useState('')
+  let [result, setResult] = React.useState([])
 
   React.useEffect(() => {
     //fetch all accounts from API
     fetch('/account/all')
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+        for (let i in data) result.push(data[i])
+        setResult(result)
         setData(JSON.stringify(data))
       })
   }, [])
   return (
     <>
       <h5>All Data in Store:</h5>
-      {data}
+      <table className="table" data={data}>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">ID</th>
+            <th scope="col">NAME</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">PASSWORD</th>
+            <th scope="col">BALANCE</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {result.map((item, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{item._id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.password}</td>
+              <td>${item.balance}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
