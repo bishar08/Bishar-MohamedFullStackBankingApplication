378a5e0e5f0a301ca55621ee17e060e2c9b694af
bishar08 <abubushayra@gmail.com>
2022-07-18 17:35:35 -0500

    my first push

---
+++ public/index.js
@@ -0,0 +1,25 @@
+function Spa() {
+  const [currentUser, setCurrentUser] = React.useState(null)
+  console.log('Current User: ' + currentUser)
+  return (
+    <HashRouter>
+      <div>
+        <NavBar />
+        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+          <div className="container" style={{ padding: '20px' }}>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/login/" component={Login} />
+            <Route path="/deposit/" component={Deposit} />
+            <Route path="/withdraw/" component={Withdraw} />
+            {/* <Route path="/transactions/" component={Transactions} /> */}
+            <Route path="/balance/" component={Balance} />
+            <Route path="/alldata/" component={AllData} />
+          </div>
+        </UserContext.Provider>
+      </div>
+    </HashRouter>
+  )
+}
+
+ReactDOM.render(<Spa />, document.getElementById('root'))
